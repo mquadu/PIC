1c989e63f18aa9648aff2726a9c701726c4d711c
Patson <74672498+CardinPatson@users.noreply.github.com>
2022-05-14 23:09:34 +0200

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,8 +10,8 @@ print("connecte au port serie: " + serialPort.portstr)
 
 # Configuration du seuil critique
 def seuilCritique():
-    resultat = spinbox1.get()
-    serialPort.write(resultat.encode())
+    resultat = int(spinbox1.get())
+    serialPort.write(bytes(":" + "%03d" % resultat, 'UTF-8'))
     time.sleep(3)
     return resultat
 
@@ -21,27 +21,31 @@ def readFromSerial():
     data = serialPort.readline().decode("ascii")
 
     if data != "":
-        # if "distance" in data: 
-            
+        # if "distance" in data:
+
         #     tab_distance = [int(s) for s in data.split() if s.isdigit()]
         #     distance = int(tab_distance[0])
-        # else : 
+        # else :
         #     distance = int (data)
         distance = str(data)
         valeur_distance = 0
-        if "distance" in distance : 
+        if "distance" in distance:
             tab_distance = [int(s) for s in data.split() if s.isdigit()]
             valeur_distance = int(tab_distance[0])
 
-        print(distance)
-
         seuil = seuilCritique()
+        print("seuil", seuil)
         if valeur_distance < int(seuil):
+            status = "OK"
             texte = f"{distance} La distance mesurée est de {valeur_distance} cm".format()
             label2.config(text=texte)
+            label3.config(text=status)
         elif valeur_distance >= int(seuil):
-            texte = f"{distance} : {valeur_distance} cm Attention le seuil critique est atteint!".format(valeur_distance)
+            status = "ALARM"
+            texte = f"{distance} cm Attention le seuil critique est atteint!".format(
+                valeur_distance)
             label2.config(text=texte)
+            label3.config(text=status)
 
     window.after(50, readFromSerial)
 
@@ -53,15 +57,19 @@ window.title("Projet Programmation PIC")
 message1 = ttk.Label(window, text="Veuillez sélectionner un seuil critique:")
 message1.grid(column=0, row=0, padx=10, pady=10)
 
-spinbox1 = ttk.Spinbox(window, from_=0, to=10, width=10, state='readonly')
-spinbox1.set(0)
+spinbox1 = ttk.Spinbox(window, from_=0, to=10, width=10, state='normal')
+spinbox1.set(100)
 spinbox1.grid(column=1, row=0, padx=10, pady=10)
 
 button1 = ttk.Button(window, text="Valider", command=seuilCritique)
 button1.grid(column=0, row=1, padx=10, pady=10)
 
-label2 = ttk.Label(window, text="", width=20)
+label2 = ttk.Label(window, text="", width=40)
 label2.grid(column=1, row=1, padx=10, pady=10)
+window.update()
+
+label3 = ttk.Label(window, text="", width=40)
+label3.grid(column=1, row=2, padx=10, pady=10)
 window.update()
 
 if __name__ == '__main__':
